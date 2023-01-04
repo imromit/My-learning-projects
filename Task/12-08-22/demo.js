442f4c9883f84b491f5754a5c12fef5ebb72ce8b
Romit <romitprince@gmail.com>
2023-01-04 16:17:53 +0530

    ..

---
+++ Task/12-08-22/demo.js
@@ -0,0 +1,38 @@
+// var a=10
+// var b = 20
+// var c = 30
+// var d=a+b+c;
+// console.log(a+"+"+b+"+"+c+"+"+d); 
+
+// console.log(`This is a backtick string
+// This is a backtick string
+// This is a backtick string
+// This is a backtick string
+// This is a backtick string
+// This is a backtick string`);
+
+// console.log(`This is a backtick string
+// This is a backtick string
+// This is a backtick string
+// This is a backtick string${a+c}
+// This is a backtick string
+// This is a backtick string`+a);
+
+// var a=20
+// var b=40
+// var c=a+b
+// console.log("The sum is "+c);
+
+// var c=b%a
+// console.log(c);
+
+// no. of days 935
+// var a=935
+// var y=parseInt (a/365)
+// var a=a%365
+// var m=parseInt (a/30)
+// var d=a%30
+
+
+
+console.log("Years = "+y+" Months = "+m+" Days = "+d);
---
+++ Task/12-09-22/demo.js
@@ -0,0 +1,64 @@
+// a= 40
+// // console.log(a+=50);
+// // console.log(a-=20);
+// // console.log(a*=10);
+// // console.log(a/=5);
+// a=10
+// console.log(a++);
+// console.log(a);
+
+// b=20
+// console.log(b--);
+// console.log(b);
+// a=10
+// console.log(++a);
+// b=20
+// console.log(--b);
+
+// console.log(5=="5");
+// console.log(5==="5");
+// console.log(5!==5);
+// console.log(5!=="5");
+// console.log(5>=5);
+// console.log(true?"hello":"ducat");
+// // console.log(false?"hello":"ducat");
+
+// var a=20
+// var b="20"
+//  console.log(a!=b?"right":"wrong");
+// console.log(5>2 && 7>9 && 12>10);
+// // console.log(5<2 || 7>9 || 12<10);
+// var a = 7  
+// var b=5
+// var c=a&b
+// var c=a|b
+// c=a^b
+// console.log(c);
+// a=10
+// b=20
+// if (a>b)
+// {
+//     console.log(a);
+// }
+// else{
+//     console.log(b);
+  var age = 15
+  if( age < 18 )
+  {
+    console.log(" You are Minor."); 
+    console.log(" Not Eligible to Work " ); 
+  }
+  else
+  {
+    if (age >= 18 && age <= 60 )
+    {
+        console.log(" You are Eligible to Work. ");
+        console.log(" Please fill in your details and apply " ); 
+    }
+    else
+    {
+        console.log(" You are too old to work as per the Government rules ");
+        console.log(" Please Collect your pension!" );    
+    }
+  }
+  
