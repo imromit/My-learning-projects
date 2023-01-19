c158895641b2741294f0c7715bf74fd6b5bbb9e9
Romit <romitprince@gmail.com>
2023-01-19 04:55:28 -0500

    Create demo.js

---
+++ Task/12-15-22/demo.js
@@ -0,0 +1,52 @@
+// var ch = "k" ;
+// switch (ch){
+//     case'a':
+//     console.log("vowel");
+//     break
+//     case'e':
+//     console.log("vowel");
+//     break;
+//     case'i':
+//     console.log("vowel");
+//     break;
+//     case'o':
+//     console.log("vowel");
+//     break;
+//     case'u':
+//     console.log("vowel");
+//     break;
+//     default:
+//     console.log("Consonant");
+// }
+// var i=10;
+// while(i<=100)
+// {
+//     console.log(i);
+//     i+=10    
+// } 
+// var i =1
+// var sum=0
+// while(i<=10){
+//     sum+=i
+//     console.log(i);
+//     i++
+    
+// }
+// console.log("total = "+sum);
+// var i =1
+// var table=2
+// while(i>=1){
+//     console.log(table+" * "+i+" = "+table*i);
+//     i++
+// }
+
+var i=1
+while(i<=10){
+    if ( i % 2 == 0 ) {
+                console.log(i+" even no.");
+    }
+    else{
+        console.log(i+" odd no.");
+    }
+    i++
+}
